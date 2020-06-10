1ff7f18abd2c7365e95bd54d9f0e16e5cac13d8e
Rahin <1015489@mborijnland.nl>
2020-06-10 15:06:54 +0200

    Contactformulier robben

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -8,18 +8,34 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('captcha', Recaptcha3Type::class, [
+            'constraints' => new Recaptcha3(),
+            'action_name' => 'homepage',
+            'script_nonce_csp' => $nonceCSP,
+        ]);
+        //$builder->add(...);
+    }
+}
+
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+
             ->add('Voornaam')
             ->add('Email', EmailType::class)
             ->add('vraag', TextareaType::class)
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
@@ -27,3 +43,5 @@ class ContactType extends AbstractType
         ]);
     }
 }
+
+
